eed8c5ab92e303d4dfea0c7f49e5ab8bf4e0f9dd
keyoanipedia@gmail.com <you@example.com>
2021-06-13 16:30:20 +0500

    added

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -11,8 +11,3 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-
-const appCheck = firebase.appCheck();
-// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
-// key is the counterpart to the secret key you set in the Firebase console.
-appCheck.activate('6LfvDi4bAAAAAHHHhHuL580-qxK89amdtKXaw2hk');
