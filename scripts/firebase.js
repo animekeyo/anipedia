f8f402bf97f65dd65806d8bee05bfb7e72e68e9d
keyoanipedia@gmail.com <you@example.com>
2021-07-06 11:10:39 +0500

    added

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,15 +1,13 @@
-var _0x81a2 = ['491941rRvWwJ', '8ANBqiv', 'AIzaSyChoUInYN6hWDmbNCuHwHTfQBPeC93pZQA', 'anime-keyo.appspot.com', '27632rBriCg', 'anime-keyo', '366264atjcFa', 'analytics', '473838311733', 'G-3YG1S4N1D6', '17639jtGVfl', '346441dHgtum', '811428bnhmJK', '1:473838311733:web:c1f0444785c7617be9d47c', '6RuSATT', 'initializeApp', '15917wFudHh', '17aiquJt'];
-var _0x3b71d9 = _0x1c69;
-
-function _0x1c69(_0x19e9d7, _0x30b3d0) { return _0x1c69 = function(_0x81a232, _0x1c69ef) { _0x81a232 = _0x81a232 - 0x72; var _0x452fcf = _0x81a2[_0x81a232]; return _0x452fcf; }, _0x1c69(_0x19e9d7, _0x30b3d0); }(function(_0x6fdccf, _0x5afe2b) {
-    var _0x5ec2f5 = _0x1c69;
-    while (!![]) {
-        try {
-            var _0x5f22a4 = -parseInt(_0x5ec2f5(0x7c)) * -parseInt(_0x5ec2f5(0x73)) + parseInt(_0x5ec2f5(0x76)) * -parseInt(_0x5ec2f5(0x80)) + -parseInt(_0x5ec2f5(0x78)) + -parseInt(_0x5ec2f5(0x7d)) + parseInt(_0x5ec2f5(0x72)) + -parseInt(_0x5ec2f5(0x82)) * parseInt(_0x5ec2f5(0x83)) + parseInt(_0x5ec2f5(0x7e));
-            if (_0x5f22a4 === _0x5afe2b) break;
-            else _0x6fdccf['push'](_0x6fdccf['shift']());
-        } catch (_0x4499f5) { _0x6fdccf['push'](_0x6fdccf['shift']()); }
-    }
-}(_0x81a2, 0x481e3));
-var firebaseConfig = { 'apiKey': _0x3b71d9(0x74), 'authDomain': 'anime-keyo.firebaseapp.com', 'databaseURL': 'https://anime-keyo-default-rtdb.firebaseio.com', 'projectId': _0x3b71d9(0x77), 'storageBucket': _0x3b71d9(0x75), 'messagingSenderId': _0x3b71d9(0x7a), 'appId': _0x3b71d9(0x7f), 'measurementId': _0x3b71d9(0x7b) };
-firebase[_0x3b71d9(0x81)](firebaseConfig), firebase[_0x3b71d9(0x79)]();
+var firebaseConfig = {
+    apiKey: "AIzaSyChoUInYN6hWDmbNCuHwHTfQBPeC93pZQA",
+    authDomain: "anime-keyo.firebaseapp.com",
+    databaseURL: "https://anime-keyo-default-rtdb.firebaseio.com",
+    projectId: "anime-keyo",
+    storageBucket: "anime-keyo.appspot.com",
+    messagingSenderId: "473838311733",
+    appId: "1:473838311733:web:c1f0444785c7617be9d47c",
+    measurementId: "G-3YG1S4N1D6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
