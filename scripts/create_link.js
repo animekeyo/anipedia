fdcc4ae902ee8cecba3ea62f7a57b1a93afc0cf2
animekeyo <79708353+animekeyo@users.noreply.github.com>
2021-07-06 09:47:19 +0500

    Delete scripts directory

--- scripts/create_link.js
+++
@@ -1,7 +0,0 @@
-const d = new Date();
-const aag = d.getTime();
-const aaf = d.getFullYear();
-const aae = d.getMonth();
-const aah = d.getMilliseconds();
-const aahx = d.getDay();
-const create_link = aag + "" + aaf + "" + aae + "" + aah + "" + aahx;
--- scripts/open_user.js
+++
@@ -1,4 +0,0 @@
-$(document).on('click', '[open_user]', function() {
-    const userid = $(this).attr('userid');
-    window.location.href = "/u?id=" + userid;
-});
